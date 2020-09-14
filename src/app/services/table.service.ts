cd573da3fcb9cb6cd7186993c331159476b2d49c
Avi Moraly <avimoraly@gmail.com>
2020-09-14 21:59:25 +0300

    fix table borders

--- src/app/services/table.service.ts
+++ src/app/services/table.service.ts
@@ -9,8 +9,9 @@ export class TableService {
   tableData = data;
   matrixdData = [];
   mapper = {};
+
   constructor() {
-    console.log('---------', this.tableData);
+    // mainly  creating data structure do use in html
     this.matrixdData[0] = [];
     this.tableData.zones.forEach((zone: Zone, i: number) => {
       this.matrixdData[i + 1] = [];
@@ -18,7 +19,6 @@ export class TableService {
         this.matrixdData[i][j + 1] = null;
       });
       this.matrixdData[0][i + 1] = zone;
-      // this.matrixdData[i + 1] = [];
       this.matrixdData[i + 1][0] = zone;
       this.mapper[zone.guid] = i + 1;
     });
@@ -27,35 +27,5 @@ export class TableService {
       const dest = this.mapper[section.destinationGuid];
       this.matrixdData[source][dest] = section;
     });
-    console.log(' this.matrixdData',  this.matrixdData)
-
-// destinationGuid: "P5"
-// sourceGuid: "P3"
-
-    // export interface Section {
-    //   complianceLevel: number;
-    //   destinationGuid: string;
-    //   hasErrors: boolean;
-    //   hasMultipleSections: boolean;
-    //   hasUncomputedResults: boolean;
-    //   sectionId: number;
-    //   sourceGuid: string;
-    //   violationsCount: number;
-    // }
-
-//     complianceLevel: 100
-// destinationGuid: "P5"
-// sourceGuid: "P3"
-// hasErrors: false
-// hasMultipleSections: false
-// hasUncomputedResults: false
-// sectionId: 3037
-// violationsCount: 0
-
-
-
-    console.log('---------', this.matrixdData);
-
-
   }
 }
